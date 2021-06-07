74a37a50c3217d19b88d97c115025c5f122f3c31
GCPeppe <guilhermecarvalho.peppe@gmail.com>
2021-06-07 18:25:56 -0300

    Reajuste na procura pela data devido à mudança de formato

--- covid-mu.py
+++ covid-mu.py
@@ -5,9 +5,7 @@ from datetime import datetime, timedelta
 import csv
 import pandas as pd
 
-hoje = datetime.today()
-inicio = datetime(2020, 5, 19)
-
+inicio, hoje = datetime(2020, 5, 19), datetime.today()
 intervalo = pd.date_range(inicio, hoje)
 
 datas = list()
@@ -19,14 +17,14 @@ obt = list()
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                          '(KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.51'}
-url = 'https://muriae.mg.gov.br/coronavirus2/'
+url = 'https://muriae.mg.gov.br/coronavirus/'
 
 request = requests.get(url, headers=headers)
 page_soup = soup(request.content, 'html.parser')
 
 conteudo = page_soup.body.div.text.split()
 
-for palavra in conteudo:
+for n, palavra in enumerate(conteudo):
     if palavra == 'Epidemiológico':
         data = conteudo.index(palavra)
         string = conteudo[data + 2][0:10]
@@ -39,19 +37,25 @@ for palavra in conteudo:
         d_type = datetime.strptime(string, '%d/%m/%Y')
         datas.append(d_type)
         conteudo.pop(data)
+    elif 'Boletim' in conteudo[n] and conteudo[n + 1] == '-':
+        data = conteudo.index(palavra)
+        string = conteudo[data + 2][0:5:] + '/2021'
+        d_type = datetime.strptime(string, '%d/%m/%Y')
+        datas.append(d_type)
+        conteudo.pop(data)
     elif palavra == 'confirmados:':
         confirmados = conteudo.index(palavra)
-        inteiro = int(conteudo[confirmados + 1].replace('.',''))
+        inteiro = int(conteudo[confirmados + 1].replace('.', ''))
         conf.append(inteiro)
         conteudo.pop(confirmados)
     elif palavra == 'covid-19:':
         confirmados = conteudo.index(palavra)
-        inteiro = int(conteudo[confirmados + 1].replace('.',''))
+        inteiro = int(conteudo[confirmados + 1].replace('.', ''))
         conf.append(inteiro)
         conteudo.pop(confirmados)
     elif palavra == 'ativos:':
         ativos = conteudo.index(palavra)
-        ativ.append(int(conteudo[ativos + 1].replace('Pacientes','')))
+        ativ.append(int(conteudo[ativos + 1].replace('Pacientes', '')))
         conteudo.pop(ativos)
     elif 'Óbitos:' in palavra:
         obitos = conteudo.index(palavra)
@@ -59,11 +63,11 @@ for palavra in conteudo:
         conteudo.pop(obitos)
     elif palavra == 'dose:':
         dose = conteudo.index(palavra)
-        dose1.append(int(conteudo[dose + 1].replace('Vacinados', '').replace('.','')))
+        dose1.append(int(conteudo[dose + 1].replace('Vacinados', '').replace('.', '')))
         conteudo.pop(dose)
     elif palavra == 'doses:':
         dose = conteudo.index(palavra)
-        dose2.append(int(conteudo[dose + 1].replace('PACIENTESTotal', '').replace('.','')))
+        dose2.append(int(conteudo[dose + 1].replace('PACIENTESTotal', '').replace('.', '')))
         conteudo.pop(dose)
     else:
         pass
@@ -71,7 +75,6 @@ for palavra in conteudo:
 with open('covid-muriae.csv', 'r', encoding='utf8') as file:
     reader = csv.DictReader(file, delimiter=';')
     for row in reader:
-
         data = row['\ufeffdata']
         d_type = datetime.strptime(data, '%d/%m/%Y')
         datas.append(d_type)
@@ -92,12 +95,12 @@ d2 = {'Data': intervalo}
 df2 = pd.DataFrame(data=d2)
 pd.set_option('display.max_rows', len(df2))
 
-
 joined = pd.merge(df2, df, on='Data', how='left').fillna(0)
 
 joined[['Dose 1', 'Dose 2', 'Casos Totais', 'Casos Ativos', 'Óbitos']] = \
-joined[['Dose 1', 'Dose 2', 'Casos Totais', 'Casos Ativos', 'Óbitos']].astype('int')
+    joined[['Dose 1', 'Dose 2', 'Casos Totais', 'Casos Ativos', 'Óbitos']].astype('int')
 
+# transformando os dados zeros em dados do dia anterior
 
 c = 1
 nao_zero = int()
@@ -125,108 +128,114 @@ pd.set_option('display.max_columns', len(joined.columns))
 
 print(joined)
 
-#joined.to_csv('C:/Users/GCPeppe/Documents/covid-muriae-auto.csv', encoding='latin-1', sep=';')
 
-
-
+# joined.to_csv('C:/Users/GCPeppe/Documents/covid-muriae-auto.csv', encoding='latin-1', sep=';')
 
 def plotar_casosVSimun(save=False, show=True):
     from numpy import max
-    plt.figure(figsize=(1080/600, 1080/600), dpi=600)
-    plt.plot(joined['Data'], joined['Dose 1'], color='green', linewidth=0.5)
-    plt.plot(joined['Data'], joined['Dose 2'], color='green', linewidth=0.5)
-    plt.plot(joined['Data'],joined['Casos Totais'], color='blue', linewidth=0.5)
+    plt.figure(figsize=(1080 / 600, 1080 / 600), dpi=600)
+    plt.plot(joined['Data'], joined['Dose 1'], color='green', linewidth=0.5, label='Dose 1 da Vacina')
+    plt.plot(joined['Data'], joined['Dose 2'], color='green', linewidth=0.5, label='Dose 2 da Vacina')
+    plt.plot(joined['Data'], joined['Casos Totais'], color='blue', linewidth=0.5, label='Casos Totais')
     plt.subplots_adjust(top=0.86, bottom=0.1, left=0.15, right=0.96)
     plt.xticks(joined['Data'][::60], fontsize=2.5, rotation=20, y=0.04)
-    plt.yticks(range(0, max(dose1), 800), fontsize=3, x=0.03)
+    plt.yticks(range(0, max(dose1), 1000), fontsize=3, x=0.03)
     plt.ylabel('População', fontsize=6, labelpad=0.1)
+    plt.legend(loc='upper left', fontsize=2)
     plt.title('Casos Totais vs Doses aplicadas em Muriaé \n'
               'segundo os boletins epidemiológicos da \n'
               'Prefeitura de Muriaé', fontsize=4, y=0.96)
 
-    if save == True:
+    if save:
         plt.savefig('C:/Users/GCPeppe/Pictures/ativos-vs-imunizados.png', dpi=600)
     else:
         pass
 
-    if show == True:
+    if show:
         plt.show()
     else:
         pass
+
 
 def plotar_casosAtivos(save=False, show=True):
     from numpy import max
 
-    plt.figure(figsize=(1080/600, 1080/600), dpi=600)
+    plt.figure(figsize=(1080 / 600, 1080 / 600), dpi=600)
     plt.plot(joined['Data'], joined['Casos Ativos'], color='orange', linewidth=0.5)
     plt.subplots_adjust(top=0.86, bottom=0.1, left=0.15, right=0.96)
     plt.yticks(range(0, max(joined['Casos Ativos']), 45), fontsize=3, x=0.03)
     plt.xticks(joined['Data'][::60], fontsize=2.5, rotation=20, y=0.04)
-    plt.ylabel('Casos Ativos', fontsize=6, labelpad=0.48)
-    plt.title('Evolução dos Casos Ativos de COVID-19 em Muriaé \n '
-          'segundo os boletins epidemiológicos \n da '
-          'Prefeitura de Muriaé', fontsize=4, y=0.96)
+    plt.ylabel('Casos Ativos', fontsize=6, labelpad=0.47)
+    plt.title(
+        'Evolução dos Casos Ativos de COVID-19 em Muriaé \n '
+        'segundo os boletins epidemiológicos \n da '
+        'Prefeitura de Muriaé', fontsize=4, y=0.96
+    )
 
-
-    if save == True:
+    if save:
         plt.savefig('C:/Users/GCPeppe/Pictures/casos_ativos.png', dpi=600)
     else:
         pass
 
-    if show == True:
+    if show:
         plt.show()
     else:
         pass
 
+
 def plotar_obitos(save=False, show=True):
     from numpy import max
 
-    plt.figure(figsize=(1080/600, 1080/600), dpi=600)
+    plt.figure(figsize=(1080 / 600, 1080 / 600), dpi=600)
     plt.bar(joined['Data'], joined['Óbitos Diários'], color='red', width=1)
     plt.subplots_adjust(top=0.86, bottom=0.1, left=0.15, right=0.96)
     plt.yticks(range(0, max(joined['Óbitos Diários']) + 1), fontsize=3, x=0.02)
     plt.xticks(joined['Data'][::60], fontsize=2.5, rotation=20, y=0.04)
     plt.ylabel('Óbitos Diários', fontsize=6, labelpad=0.48)
-    plt.title('Evolução dos Óbitos por COVID-19 em Muriaé \n '
-          'segundo os boletins epidemiológicos \n da '
-          'Prefeitura de Muriaé', fontsize=4, y=0.96)
+    plt.title(
+        'Óbitos diários por COVID-19 em Muriaé \n '
+        'segundo os boletins epidemiológicos \n '
+        'da Prefeitura de Muriaé', fontsize=4, y=0.96
+    )
 
-
-    if save == True:
+    if save:
         plt.savefig('C:/Users/GCPeppe/Pictures/covid_obitos.png', dpi=600)
     else:
         pass
 
-    if show == True:
+    if show:
         plt.show()
     else:
         pass
 
+
 def plotar_casos_diarios(save=False, show=True):
     from numpy import max
 
-    plt.figure(figsize=(1080/600, 1080/600), dpi=600)
+    plt.figure(figsize=(1080 / 600, 1080 / 600), dpi=600)
     plt.bar(joined['Data'], joined['Casos Diários'], color='blue', width=1)
     plt.subplots_adjust(top=0.86, bottom=0.1, left=0.15, right=0.96)
     plt.yticks(range(0, max(joined['Casos Diários']), 20), fontsize=3, x=0.02)
     plt.xticks(joined['Data'][::60], fontsize=2.5, rotation=20, y=0.04)
     plt.ylabel('Casos Diários', fontsize=6, labelpad=0.48)
-    plt.title('Evolução dos Óbitos por COVID-19 em Muriaé \n '
-          'segundo os boletins epidemiológicos \n da '
-          'Prefeitura de Muriaé', fontsize=4, y=0.96)
+    plt.title(
+        'Casos Diários de COVID-19 em Muriaé \n '
+        'segundo os boletins epidemiológicos \n da '
+        'Prefeitura de Muriaé', fontsize=4, y=0.96
+    )
 
-
-    if save == True:
+    if save:
         plt.savefig('C:/Users/GCPeppe/Pictures/casos_diarios.png', dpi=600)
     else:
         pass
 
-    if show == True:
+    if show:
         plt.show()
     else:
         pass
 
-#plotar_casosAtivos()
-#plotar_casosVSimun()
-plotar_obitos()
-#plotar_casos_diarios()
+
+plotar_casosAtivos(save=True)
+plotar_casosVSimun(save=True)
+plotar_obitos(save=True)
+plotar_casos_diarios(save=True)

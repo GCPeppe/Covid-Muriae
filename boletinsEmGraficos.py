2c8e203192a1d079e2db7d95752abb7cbb55f764
GCPeppe <guilhermecarvalho.peppe@gmail.com>
2021-07-14 15:22:36 -0300

    simplificadas as buscas por palavras precedentes  aos dados

--- boletinsEmGraficos.py
+++ boletinsEmGraficos.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import requests
 from bs4 import BeautifulSoup as soup
 from matplotlib import pyplot as plt
@@ -5,7 +11,11 @@ from datetime import datetime, timedelta
 import csv
 import pandas as pd
 
-inicio, hoje = datetime(2020, 5, 19), datetime.today()
+
+# In[2]:
+
+
+inicio, hoje = datetime(2020, 5, 19), datetime.today().date()
 intervalo = pd.date_range(inicio, hoje)
 
 datas = list()
@@ -24,6 +34,8 @@ page_soup = soup(request.content, 'html.parser')
 
 conteudo = page_soup.body.div.text.split()
 
+chaves = ['Epidemiológico']
+
 for n, palavra in enumerate(conteudo):
     if palavra == 'Epidemiológico':
         string = conteudo[n + 2][0:10]
@@ -35,8 +47,7 @@ for n, palavra in enumerate(conteudo):
         d_type = datetime.strptime(string, '%d/%m/%Y')
         datas.append(d_type)
         conteudo.pop(n)
-    elif ('Boletim' in conteudo[n] and conteudo[n + 1] == '-') \
-            or (conteudo[n] == 'Covid' and conteudo[n + 1] == '-'):
+    elif ('Boletim' in conteudo[n] and conteudo[n + 1] == '-')     or (conteudo[n] == 'Covid' and conteudo[n+1] == '-'):
         string = conteudo[n + 2][0:5:] + '/2021'
         d_type = datetime.strptime(string, '%d/%m/%Y')
         datas.append(d_type)
@@ -51,36 +62,44 @@ for n, palavra in enumerate(conteudo):
     elif 'Óbitos:' in palavra:
         obt.append(int(conteudo[n + 1].replace('Investigados', '')))
         conteudo.pop(n)
-    elif ('Primeira' in conteudo[n] and conteudo[n + 1] == 'dose:') or \
-            (conteudo[n] == 'uma' and conteudo[n + 1] == 'dose:'):
+    elif ('Primeira' in conteudo[n] and conteudo[n+1] == 'dose:') or                         (conteudo[n] == 'uma' and conteudo[n+1] == 'dose:') or                         ( 'uma' in conteudo[n] and conteudo[n+1] == 'dose):') or                         ('Parcialmente' in conteudo[n] and conteudo[n+1] == 'vacinados:'):
         dose1.append(int(conteudo[n + 2].replace('Vacinados', '').replace('.', '')))
         conteudo.pop(n)
-    elif palavra == 'doses:' or palavra == 'única:':
+    elif palavra == 'doses:' or palavra == 'única:' or palavra == 'única):':
         dose2.append(int(conteudo[n + 1].replace('PACIENTESTotal', '').replace('.', '')))
         conteudo.pop(n)
-    elif 'Segunda' in conteudo[n] and conteudo[n + 1] == 'dose:':
-        dose2.append(int(conteudo[n + 2].replace('.', '')))
+    elif 'Segunda' in conteudo[n] and conteudo[n+1] == 'dose:' or                 ( 'Totalmente' in conteudo[n] and conteudo[n+1] == 'vacinados:'):
+        dose2.append(int(conteudo[n+2].replace('.','')))
         conteudo.pop(n)
     else:
         pass
 
-with open('covid-muriae.csv', 'r', encoding='utf8') as file:
-    reader = csv.DictReader(file, delimiter=';')
-    for row in reader:
-        data = row['\ufeffdata']
-        d_type = datetime.strptime(data, '%d/%m/%Y')
-        datas.append(d_type)
+print(len(datas), len(dose1), len(dose2), len(conf), len(ativ), len(obt))
 
-        dose1.append(int(row['dose 1']))
-        dose2.append(int(row['dose 2']))
-        conf.append(int(row['casos_totais']))
-        ativ.append(int(row['casos_ativos']))
-        obt.append(int(row['óbitos']))
 
+# In[3]:
+
+
+datas
+
+
+# In[4]:
+
+
+bkup = pd.read_csv('covid-muriae-auto.csv', sep=';', encoding='Latin1')
+
+bkup['Data'] = [datetime.strptime(data, '%d/%m/%Y') for data in bkup['Data']]
 d = {'Data': datas, 'Dose 1': dose1, 'Dose 2': dose2,
      'Casos Totais': conf, 'Casos Ativos': ativ, 'Óbitos': obt}
-
 df = pd.DataFrame(data=d)
+df = pd.concat([bkup, df], axis=0)
+
+df['Data']
+
+
+# In[5]:
+
+
 pd.set_option('display.max_rows', len(df))
 
 d2 = {'Data': intervalo}
@@ -89,8 +108,7 @@ pd.set_option('display.max_rows', len(df2))
 
 joined = pd.merge(df2, df, on='Data', how='left').fillna(0)
 
-joined[['Dose 1', 'Dose 2', 'Casos Totais', 'Casos Ativos', 'Óbitos']] = \
-    joined[['Dose 1', 'Dose 2', 'Casos Totais', 'Casos Ativos', 'Óbitos']].astype('int')
+joined[['Dose 1', 'Dose 2', 'Casos Totais', 'Casos Ativos', 'Óbitos']] =     joined[['Dose 1', 'Dose 2', 'Casos Totais', 'Casos Ativos', 'Óbitos']].astype('int')
 
 # transformando os dados zeros em dados do dia anterior
 
@@ -102,7 +120,7 @@ while True:
         if value != 0:
             nao_zero = value
         else:
-            if joined.loc[i, joined.columns[0]] != hoje.date():
+            if joined.loc[i, joined.columns[0]] != hoje:
                 joined.loc[i, joined.columns[c]] = nao_zero
             else:
                 joined.drop(i, inplace=True)
@@ -112,13 +130,19 @@ while True:
         if c == len(joined.columns):
             break
 
+joined.at[456, 'Óbitos'] = 301
+
+
 joined['Casos Diários'] = joined['Casos Totais'].diff().fillna(0).astype('int')
 joined['Óbitos Diários'] = joined['Óbitos'].diff().fillna(0).astype('int')
 
 pd.set_option('display.max_columns', len(joined.columns))
 
-plt.rcParams.update({'figure.facecolor': '#92b69e'})
-plt.rcParams['axes.linewidth'] = 0.1
+pd.set_option('display.max_rows', len(joined))
+joined
+
+
+# In[6]:
 
 
 def plotar_casosVSimun(save=False, show=True):
@@ -128,22 +152,27 @@ def plotar_casosVSimun(save=False, show=True):
     ax.set(facecolor='#c6e2ff')
     plt.plot(joined['Data'], joined['Dose 1'], color='#8dae56', linewidth=0.5, label='Dose 1 da Vacina')
     plt.plot(joined['Data'], joined['Dose 2'], color='green', linewidth=0.5, label='Dose 2 e dose única da Vacina')
-    plt.plot(joined['Data'], joined['Casos Totais'], color='blue', linewidth=0.5, label='Casos Totais')
     plt.subplots_adjust(top=0.86, bottom=0.1, left=0.15, right=0.96)
-    plt.xticks(joined['Data'][::60], fontsize=2.5, rotation=20, y=0.04)
+    plt.xticks(joined['Data'][::60], fontsize=2, rotation=20, y=0.04)
     plt.xlim(min(joined['Data']), max(joined['Data']))
-    plt.ylim(0, max(joined['Dose 1']))
-    plt.yticks(range(0, max(dose1), 3000), fontsize=3, x=0.03)
-    plt.ylabel('Doses', fontsize=6, labelpad=0.9)
-    plt.legend(loc='upper left', fontsize=2)
-    plt.title('Casos Totais vs Doses aplicadas em Muriaé \n'
+    plt.ylim(0, max(joined['Dose 1'] + 16000))
+    plt.yticks(range(0, max(dose1), 6000), fontsize=3, x=0.03)
+    plt.ylabel('Doses', fontsize=2, labelpad=0.8)
+    plt.legend(loc='upper right', fontsize=2)
+    
+    ax2 = plt.gca().twinx()
+    plt.ylabel('Casos Ativos', fontsize=2, labelpad=0.8)
+    plt.yticks(fontsize=2)
+    plt.xlim(min(joined['Data']), max(joined['Data']) + timedelta(days=1))
+    plt.plot(joined['Data'], joined['Casos Ativos'], color='orange', linewidth=0.5)
+    plt.title('Casos Ativos vs Doses aplicadas em Muriaé \n'
               'segundo os boletins epidemiológicos da \n'
-              'Prefeitura de Muriaé', fontsize=4, y=0.96)
-
+              'Prefeitura de Muriaé', fontsize=2.5, y=0.96)
+    plt.tight_layout()
     if save:
-
-        plt.savefig('C:/Users/GCPeppe/Pictures/ativos-vs-imunizados.png', dpi=600)
-
+        
+        plt.savefig('C:/Users/GCPeppe/Pictures/ativos-vs-imunizados.png')
+        
     else:
         pass
 
@@ -192,7 +221,7 @@ def plotar_obitos(save=False, show=True):
     plt.subplots_adjust(top=0.86, bottom=0.1, left=0.15, right=0.96)
     plt.yticks(range(0, max(joined['Óbitos Diários']) + 1), fontsize=3, x=0.02)
     plt.xlim(min(joined['Data']), max(joined['Data']) + timedelta(days=1))
-    plt.xticks(joined['Data'][::60], fontsize=2.5, rotation=20, y=0.04)
+    plt.xticks(joined['Data'][::90], fontsize=2.5, rotation=20, y=0.04)
     plt.ylabel('Óbitos Diários', fontsize=6, labelpad=0.48)
     plt.title(
         'Óbitos diários por COVID-19 em Muriaé \n '
@@ -217,7 +246,7 @@ def plotar_casos_diarios(save=False, show=True):
     plt.figure(figsize=(1080 / 600, 1080 / 600), dpi=600)
     ax = plt.axes()
     ax.set(facecolor='#c6e2ff')
-    plt.bar(joined['Data'], joined['Casos Diários'], color='blue', width=1)
+    plt.bar(joined['Data'], joined['Casos Diários'], color='blue')
     plt.subplots_adjust(top=0.86, bottom=0.1, left=0.15, right=0.96)
     plt.yticks(range(0, max(joined['Casos Diários']), 20), fontsize=3, x=0.02)
     plt.xticks(joined['Data'][::60], fontsize=2.5, rotation=20, y=0.04)
@@ -239,9 +268,8 @@ def plotar_casos_diarios(save=False, show=True):
     else:
         pass
 
-
 def relacao_ativosObtos(save=False, show=True):
-    plt.figure(figsize=(12, 12))
+    plt.figure(figsize=(12,12))
     ax = plt.axes()
     ax.set(facecolor='#c6e2ff')
     plt.bar(joined['Data'], joined['Óbitos Diários'], color='red')
@@ -262,11 +290,84 @@ def relacao_ativosObtos(save=False, show=True):
         plt.show()
     else:
         pass
+    
 
 
-print(joined.tail())
+# In[7]:
 
-#plotar_casosAtivos(save=True)
-#plotar_casosVSimun(save=True)
-#plotar_obitos(save=True)
-#plotar_casos_diarios(save=True)
+
+joined.to_csv('C:/Users/GCPeppe/Documents/covid-muriae-auto.csv', encoding='utf-8', sep=';')
+
+
+# In[8]:
+
+
+plt.rcParams.update({'figure.facecolor': '#92b69e'})
+plt.rcParams['axes.linewidth'] = 0.1
+
+
+# In[9]:
+
+
+plotar_casosAtivos(save=True)
+
+
+# In[10]:
+
+
+plotar_casosVSimun(save=True)
+
+
+# In[11]:
+
+
+plotar_obitos(save=True)
+
+
+# In[12]:
+
+
+plotar_casos_diarios(save=True)
+
+
+# In[13]:
+
+
+relacao_ativosObtos(save=True)
+
+from Google import Create_service 
+    
+FILE = 'client_secret_GoogleCloudDemo.json'
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis./auth/drive']
+
+service = Create_Service(FILE, API_NAME, API_VERSION, SCOPES)
+
+folder_id = '1r6363ZH-r7S6HcJlXOSIq9wA5biKoMen'
+file_names = [
+        'ativos-vs-imunizados.png',
+        'casos_ativos.png', 
+        'casos_diarios.png',
+        'covid_obitos.png'
+                ]
+file_types = [
+        'image/png',
+        'image/png',
+        'image/png',
+        'image/png'
+    ]
+    
+for file_name, file_type in zip(file_names, file_types):
+    metadata = {
+        'name': file_name,
+        'parents': [folder_id]
+    }
+
+    media = MediaFileUpload('C:/Users/GCPeppe/Pictures/{0}'.format(file_name), mimetype=file_type)
+    
+    service.files().create(
+        body=metadata,
+        media_body=media,
+        fields='id'
+    ).execute()
